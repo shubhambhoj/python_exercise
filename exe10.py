5f351d951abd952350137569e329a4834c26d967
shubhambhoj <shubhambhoj75@gmail.com>
2022-09-08 22:57:21 +0530

    add exe 10,11,12

---
+++ exe10.py
@@ -0,0 +1,19 @@
+print("I am 6'2\" tall.") # use of Double-quote in Double-quote using \
+print('I am 6\'2" tall.') # use of Single-quote in Single-quote using \
+
+tabby_cat = "\tI'm tabbed in." # use of TAB escape sequences
+persian_cat = "I'm split\non a line." # use of NEW LINE escape sequences
+backslach_cat = "I'm \\ a \\ cat." # use of BLACKSLACH escape sequences
+
+
+fat_cat = """
+I'll do a list:     
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslach_cat)
+print(fat_cat)
---
+++ exe12.py
@@ -0,0 +1,4 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+print(f"so,you're {age} old, {height} cm tall and {weight} kg heavy.")
