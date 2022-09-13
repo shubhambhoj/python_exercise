c327a7180afe43a9191c8922fefff054a49b6088
shubhambhoj <shubhambhoj75@gmail.com>
2022-09-13 21:48:26 +0530

    add exercise 33

---
+++ exe33.py
@@ -0,0 +1,16 @@
+
+i = 0               # initializing i
+numbers = []        # creating empty list
+
+while i < 6 :       # syntax of while loop
+    print(f" At the top i is {i}")
+    numbers.append(i)
+
+    i += 1
+    print("numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+print("The numbers: ") 
+
+for num in numbers :
+    print(num)
