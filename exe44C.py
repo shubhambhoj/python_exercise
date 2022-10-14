4c678cbb6282408f00043b74bab63f3712e75cc2
shubhambhoj <shubhambhoj75@gmail.com>
2022-10-14 13:11:36 +0530

    exe44c modify

--- exe44C.py
+++ exe44C.py
@@ -1,19 +1,19 @@
 class Parent(object):
 
     def altered(self):
-        #self.a = m
-        #print(self.a)
+    
         print("PARENT altered() function")
 
 class Child(Parent):
 
     def altered(self):
         print("CHILD, BEFORE PARENT altered() function")
-        #super(child, self).altered()
-        super().altered()
+        #super(child, self).altered()               # it compelsory in python 2 to give arg.in super()
+        super().altered()                           # in python3 it is optional so if we not it take default arg.
         print("CHILD, AFTER PARENT altered() function")
 
 dad = Parent()
 son = Child()
+
 dad.altered()
 son.altered()
